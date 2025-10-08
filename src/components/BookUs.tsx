b8ba16b83b05977b212ec78ede27ed3381d05897
Jarne-Wils <jarne.wils@flux.be>
2025-10-08 10:18:52 +0200

    email integration

--- src/components/BookUs.tsx
+++ src/components/BookUs.tsx
@@ -1,12 +1,18 @@
 function BookUs() {
 
+    const handleClick = () => {
+        window.open(
+        "https://mail.google.com/mail/?view=cm&fs=1&to=dampbookings@gmail.com",
+        "_blank"
+        );
+    };
+
   return (
-    <div>
-        {/* <a href="#"> */}
+    <div onClick={handleClick} style={{ cursor: "pointer" }}>
+        {/* <a href="mailto:dampbookings@gmail.com" className="book-link"> */}
             <div className="book-container">
                 <p className="book-text">
-                    {/* Book us {'>'} */}
-                    * * *
+                    Book us {'>'}
                 </p>
             </div>
         {/* </a> */}
