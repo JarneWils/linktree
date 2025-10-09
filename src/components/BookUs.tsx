1c6ba06d11494efbc4c41d3c8dd477104550b9f8
Jarne-Wils <jarne.wils@flux.be>
2025-10-09 10:49:26 +0200

    cursor

--- src/components/BookUs.tsx
+++ src/components/BookUs.tsx
@@ -1,24 +1,17 @@
 function BookUs() {
-
-    const handleClick = () => {
-        window.open(
-        "https://mail.google.com/mail/?view=cm&fs=1&to=dampbookings@gmail.com",
-        "_blank"
-        );
-    };
+  const handleClick = () => {
+    window.location.href = "mailto:dampbookings@gmail.com";
+  };
 
   return (
     <div onClick={handleClick} style={{ cursor: "pointer" }}>
-        {/* <a href="mailto:dampbookings@gmail.com" className="book-link"> */}
-            <div className="book-container">
-                <p className="book-text">
-                    Book us {'>'}
-                </p>
-            </div>
-        {/* </a> */}
+      <div className="book-container">
+        <p className="book-text">
+          Book us {'>'}
+        </p>
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default BookUs
+export default BookUs;
--- src/components/Platforms.tsx
+++ src/components/Platforms.tsx
@@ -6,10 +6,10 @@ function Platforms() {
   return (
     <div className="platforms-container">
       <a href="https://soundcloud.com/damp-3670?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" className="soundcloud-container" target="_blank">
-          <SiSoundcloud  className="soundcloud-icon" size={100}/>
+          <SiSoundcloud  className="soundcloud-icon" size={70}/>
       </a>
       <a href="https://open.spotify.com/artist/3s4SjE8neU2R1WIUE09rkC?si=P6PHDpI2SsOoCmEeiyZRzQ" className="spotify-container" target="_blank">
-        <SiSpotify className="spotify-icon" size={80}/>
+        <SiSpotify className="spotify-icon" size={50}/>
       </a>
     </div>
   )
